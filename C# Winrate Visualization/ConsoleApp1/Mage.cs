afb9eb7cb210af2a5e3475c41af1328064c97d8d
zVyxx <sztefankracsica0@gmail.com>
2025-05-14 12:42:20 +0200

    visualizer

---
+++ C# Winrate Visualization/ConsoleApp1/Mage.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    public class Mage : GameCharacter
+    {
+
+        public Mage()
+        {
+            name = "Mage";
+            health = random.Next(50, 100);
+            damage = random.Next(25, 50);
+        }
+
+        public Mage(string name, double health, double damage, int id) 
+        {
+            this.name = name;
+            this.health = health;
+            this.damage = damage;
+            this.id = id;
+        }
+    }
+}
---
+++ C# Winrate Visualization/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameController.addUndead(100);
+            GameController.addMage(100);
+            GameController.startBattle();
+            GameController.showWinrate();
+        }
+    }
+}
